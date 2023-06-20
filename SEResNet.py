45aa8891c51e9f12ec783a254c16f12dc44a44b2
Wang Han <66519887+happywind-boy@users.noreply.github.com>
2023-06-20 12:28:08 +0800

    Update SEResNet.py

--- SEResNet.py
+++ SEResNet.py
@@ -293,6 +293,8 @@ class CifarSEPreActResNet(CifarSEResNet):
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
+    
+    return x
 
 
 def se_resnet20(**kwargs):
